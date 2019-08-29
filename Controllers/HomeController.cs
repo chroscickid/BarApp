861172765bd8a5b44e65ee3722ada8e25aa86d0f
chroscickid <44069349+chroscickid@users.noreply.github.com>
2019-08-28 22:02:57 -0400

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using BrApp.Models;
+using HtmlAgilityPack;
+
+namespace BrApp.Controllers
+{
+     
+    public class HomeController : Controller
+    {
+        //Whenever a drink is ordered, we create an ID for it to keep track of the total number of drinks
+        private static int id = 0;
+        
+        //the list of drinks that are ordered
+        public static List<Drink> Orders = new List<Drink>();
+
+        //the list of drinks that can be ordered
+        private static List<Drink> drinks
+            = new List<Drink>()
+            {
+                new Drink {Name = "Margarita", Description = "Salt and tequila!!!", Price = 5},
+                new Drink {Name = "Bloody Mary", Description = "Whisper it 3 times in the bathroom in the dark", Price = 7},
+                new Drink {Name = "Miller Lite", Description = "It's miller time", Price = 4},
+                new Drink {Name = "Water", Description = "For the designated driver", Price = 0}
+                
+            };
+
+       
+        //Takes the user to the front page
+        public ActionResult Index()  
+        {
+            return View(drinks);
+        }
+
+
+        //The method used to place an order. It receives the value "p" from Index.cshtml
+        //It then adds an ID to the drink (You can see that this is a value of the drink model in drink.cs
+
+        public RedirectToRouteResult Order(Drink drink) 
+        {
+            drink.Id = ++id;
+            Orders.Add(drink);
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
+
---
+++ Controllers/QueueController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using BrApp.Models;
+
+namespace BrApp.Controllers
+{
+   
+    public class QueueController : Controller
+    {
+        //Creates a list of ready drinks
+        private static List<Drink> ready = new List<Drink>();
+
+        //Shows the ViewOrders.cshtml view
+        public ActionResult ViewOrders () 
+
+        {
+            return View(HomeController.Orders);
+        }
+
+        //This method is called from the "View Orders Table"
+        //First, we add the "drink" item that we passed through to the list of prepared drinks
+        //Then we remove it from the list of pending orders
+       public RedirectToRouteResult Prepare(Drink drink)
+        {
+            ready.Add(drink);
+
+            
+            var send = HomeController.Orders.Where(x => x.Id == drink.Id); // x rep an object in Orders            
+            HomeController.Orders.Remove(send.FirstOrDefault());
+
+            return RedirectToAction("ViewOrders");  
+        }
+
+        //This removes the "drink" item from the list of pending orders, without doing anything else
+        public RedirectToRouteResult Cancel(Drink drink)
+        {
+            var cancel = HomeController.Orders.Where(x => x.Id == drink.Id);             
+            HomeController.Orders.Remove(cancel.FirstOrDefault());
+
+            return RedirectToAction("ViewOrders");
+        }
+
+
+
+
+
+        public ActionResult ViewPrepared() 
+
+        {
+            return View(ready); 
+        }
+
+        
+        public RedirectToRouteResult Served(Drink drink)
+        {
+            ready.RemoveAll(x => x.Id == drink.Id);
+            return RedirectToAction("ViewPrepared");
+        }
+
+    }
+}
